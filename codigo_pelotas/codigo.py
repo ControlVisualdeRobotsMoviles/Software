6824e0bd00f245a931ba6ddad0705ed8963ee138
unknown <eduardonunes.167@gmail.com>
2018-07-01 20:10:59 -0400

    data organizada

--- codigo_pelotas/codigo.py
+++ codigo_pelotas/codigo.py
@@ -21,19 +21,35 @@ class Carro:
         self.y2 = 0
         self.centrox = 0
         self.centroy = 0
+        self.colorBack = "vacio"
+        self.colorFront = "vacio"
 
     def centro(self):
-        self.centrox = self.x1 - self.x2
-        self.centroy = self.y1 - self.y2
-       
-def set_pam(data_serv,elementos,bola,carro):
-    for i in range(0,elementos):
-        lista.append(Bolas())
+        self.centrox = (self.x1 + self.x2)/2
+        self.centroy = (self.y1 + self.y2)/2
 
-lista = [Bola()]
+
+def set_pam(data_serv,elementos,carro,lista):
+    cont  = 0
+    for i in range(0,elementos):
+        if (data_serv[i][2]==carro.colorBack): 
+            pass
+        elif (data_serv[i][2] == carro.colorFront):
+            pass
+        else:
+            lista.append(Bolas())
+            lista[cont].x = data_serv[cont][0][0]
+            lista[cont].y = data_serv[cont][0][1]
+            lista[cont].radio = data_serv[cont][1]
+            lista[cont].color = data_serv[cont][2]
+            cont+=1
+
+
 
 
 if __name__ == "__main__":
+    
+    robot = Carro()
     # Define the IP address and Port of the Server.
     ip_address = "127.0.0.1"
     port = "8000"
@@ -45,6 +61,8 @@ if __name__ == "__main__":
     # Print the response
 
     num_elementos = len(response)
+    bolas = []
+    set_pam(response,num_elementos,robot,bolas)
 
     print ("tipo de dato: "+ str(type(response)) +'\n')
     '''
@@ -54,3 +72,4 @@ if __name__ == "__main__":
 
     '''
     print(response)
+    print(bolas[0].color)
